8d37194c1167c94d41a680afeaa05916a86fe2f7
Lazar Erić <lazareric2000@gmail.com>
2023-01-29 01:04:36 +0100

    without

--- julia_pia_projekat/backend/src/models/linija.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const Schema = mongoose.Schema;
-
-let Linija = new Schema({
-    broj_linije: {type:Number}
-})
-
-export default mongoose.model('Linija', Linija, 'linije');
