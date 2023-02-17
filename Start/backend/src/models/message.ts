faeea5b96f19b7e59b1e773f01edfce000e22ff9
Lazar Erić <lazareric2000@gmail.com>
2023-02-17 11:26:42 +0100

    messages

---
+++ Start/backend/src/models/message.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema;
+
+let Message = new Schema({
+    from: {type:String},
+    to: {type:String},
+    datum: {type:Date},
+    tekst: {type:String},
+    fromImg: {type:String},
+    toImg: {type:String}
+
+})
+
+export default mongoose.model('Message', Message, 'messages');
